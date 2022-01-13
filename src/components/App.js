1145edeca2feb857479491894d548d13d9a12833
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-01-13 18:07:11 -0500

    commit tree 09b0dd3b7099a75d237e5d44b89e757072bc3f35

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+    .then(r => r.json())
+    .then(r => setItems(r))
+  }, [])
+
+  function handleDelete(id){
+    const newList = items.filter(item => item.id !== id)
+    fetch(`http://localhost:6001/listings/${id}`,{
+      method: "DELETE",
+    })
+    setItems(newList)
+  }
+
+  console.log(items)
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={items} handleDelete={handleDelete}/>
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,14 +1,31 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
-  return (
-    <main>
-      <ul className="cards">
-        {/* use the ListingCard component to display listings */}
-      </ul>
-    </main>
-  );
+function ListingsContainer({listings, handleDelete}) {
+
+  function onDelete(id){
+    handleDelete(id)
+  }
+
+  if (!listings){
+    return (
+      <h2>Listings Loading...</h2> 
+    )
+  }
+
+  if (listings){
+    return (
+      <main>
+        <ul className="cards">
+          {listings.map((listing) => (
+            <ListingCard key={listing.id} listing={listing} onDelete={onDelete}/>
+          )
+          )}
+        </ul>
+      </main>
+    );
+  }
+  
 }
 
 export default ListingsContainer;
